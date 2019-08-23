37f95fc476a95516d9e90cdcab21fc36d2b6b6f5
thalitachr <52938599+thalitachr@users.noreply.github.com>
2019-08-23 00:11:01 -0300

    Add files via upload

---
+++ figuraGeometrica.cpp
@@ -0,0 +1,21 @@
+#include "figuraGeometrica.h"
+
+figuraGeometrica::figuraGeometrica()
+{
+    nome = "sem nome";
+}
+
+//void figuraGeometrica::setNome(std::string)
+//{
+//    this->nome = nome;
+//}
+
+std::string figuraGeometrica::getNome()
+{
+    return nome;
+}
+
+/*double figuraGeometrica::calcularArea()
+{
+    //abstrato puro
+}*/
